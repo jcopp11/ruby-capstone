39b75220d40cd5328c6118787a17ed0be5a56acb
Jason Coppedge <jason.p.coppedge@gmail.com>
2017-04-25 14:33:22 -0400

    Redid Cities and States generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :states, except: [:new, :edit]
+  resources :cities, except: [:new, :edit]
   root 'cities#index', defaults: {format: :json}
 
   scope :api, defaults: {format: :json} do
