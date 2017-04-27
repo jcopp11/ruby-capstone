6a3f770bf37cffcaa5da7c4c463e98a57039d0df
Jason Coppedge <jason.p.coppedge@gmail.com>
2017-04-27 17:12:47 -0400

    Fixed root and application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+	  scope :api, defaults: {format: :json} do
   resources :states, except: [:new, :edit]
   resources :cities, except: [:new, :edit]
-  root 'cities#index', defaults: {format: :json}
+  
 
-  scope :api, defaults: {format: :json} do
       end
 
   get '/ui'  => 'ui#index'
   get '/ui#'  => 'ui#index'
+  root "ui#index"
 end
